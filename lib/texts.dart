e62ab90da95055ed9bb1382cd4bbe8aa4cb699fe
nailysik <nailysik@gmail.com>
2024-10-01 15:33:44 +0300

    added uz,ar translate

--- lib/texts.dart
+++ lib/texts.dart
@@ -1,6 +1,6 @@
 /// index [EN,RU,...]
 int _ = 1;
-const _langs = {'en': 0, 'ru': 1, 'de': 2, 'kk': 3,};
+const _langs = {'en': 0, 'ru': 1, 'de': 2, 'kk': 3, 'uz': 4, 'ar': 5};
 
 Texts get txt => Texts.instance;
 
@@ -21,83 +21,107 @@ class Texts {
     'Disconnected',
     'Не подключено',
     'Keine Verbindung',
-    'Ажыратылған'
+    'Қосылмады',
+    'Uzilldi',
+    'غير متصل',
   ][_];
 
   final connecting = [
     'Connecting',
     'Подключение',
     'Verbindung',
-    'Қосылуда'
+    'Қосылу',
+    'Ulanish',
+    'التوصيل',
   ][_];
 
   final fetching = [
     'Fetching',
     'Обновление',
     'Aktualisierung',
-    'Жаңарту'
+    'Жаңарту',
+    'Yangilash',
+    'تحديث',
   ][_];
 
   final disconnecting = [
     'Disconnecting',
     'Отключение',
     'Abbruch',
-    'Ажыратылуда'
+    'Ажырату',
+    'Ajratish',
+    'قطع الاتصال',
   ][_];
 
   final problemsTitle = [
     'Connection problem',
     'Проблема с соединением',
     'Verbindungsprobleme',
-    'Қосылым мәселесі'
+    'Қосылу мәселесі',
+    'Ulanish muammosi',
+    'مشكلة الاتصال',
   ][_];
 
   final maintenanceTitle = [
     'Service maintenance',
     'Обслуживание системы',
     'Systemwartung',
-    'Жүйеге техникалық қызмет көрсету'
+    'Сервистік қызмет көрсету',
+    "Xizmat ko'rsatish",
+    'صيانة الخدمة',
   ][_];
 
   final secsBeforeReconnect = [
     'Retrying after',
     'Повторная попытка через',
     'Erneuter Versuch nach',
-    'Кейін қайталау'
+    'Кейін қайталау',
+    'Keyin qayta urinish',
+    'إعادة المحاولة بعد',
   ][_];
 
   final searchingTitle = [
     'Network waiting',
     'Ожидание сети',
     'Warte auf Netz',
-    'Желіні күту'
+    'Желіні күту',
+    'Tarmoq kutmoqda',
+    'شبكة الانتظار',
   ][_];
 
   final searchingSubtitle = [
     'Provide access',
     'Обеспечьте доступ',
     'Zugang gewähren',
-    'Қол жеткізуді қамтамасыз ету'
+    'Рұқсат беру',
+    "Kirishni ta'minlash",
+    'توفير إمكانية الوصول',
   ][_];
 
   final lastSyncPrefix = [
     'Synchronized',
     'Cинхронизировано',
     'Synchroniziert',
-    'Синхрондалған'
+    'Синхрондалған',
+    'Sinxronlashtirildi',
+    'متزامن',
   ][_];
 
   final idle = [
     'Connected',
     'Подключено',
     'Verbunden',
-    'Қосылды'
+    'Қосылған',
+    'Ulangan',
+    'متصل',
   ][_];
   final fetchedCounts = [
     'Pieces downloaded',
     'Скачано данных',
     'Heruntergeladene Stücke',
-    'Деректер жүктелді'
+    'Бөліктер жүктелді',
+    'Yuklab olingan qismlar',
+    'القطع التي تم تنزيلها',
   ][_];
 
   final searchingExplanation = [
@@ -120,16 +144,27 @@ Bitte mindestens einen Internetzugangskanal zur Verfügung stellen.
 Wenn der Flugzeugmodus aktiviert ist, ist das Mobilfunknetz nicht verfügbar, aber Sie können, falls verfügbar, WLAN-Verbindungen verwenden.
 Wenn Ihr Gerät über Mechanismen zur Beschränkung des Netzwerkzugriffs verfügt,
 stellen Sie sicher, dass alle Berechtigungen für die Anwendung erteilt werden.
-Bitte sicherstellen, dass das System keine Einschränkungen für den Zugang zum Internet hat.
-
-Übersetzt mit www.DeepL.com/Translator (kostenlose Version)
-    ''',
-    '''Қазіргі уақытта Wi-Fi немесе ұялы байланыс жоқ.
-Кем дегенде бір интернетке кіру арнасын қамтамасыз етіңіз.
-Ұшақ режимі қосылса, ұялы желі қолжетімсіз болады,
-бірақ бар болса, Wi-Fi қосылымдарын пайдалануға болады.
-Құрылғыңызда желіге кіруді шектеу механизмдері болса,
-қолданбаға барлық рұқсаттардың берілгенін тексеріңіз.
-Жүйеде интернетке кіруге ешқандай шектеулер жоқ екеніне көз жеткізіңіз'''
+Bitte sicherstellen, dass das System keine Einschränkungen für den Zugang zum Internet hat.''',
+    '''لا توجد حالياً شبكة واي فاي أو اتصال خلوي.
+توفير قناة اتصال إنترنت واحدة على الأقل.
+إذا تم تمكين وضع الطائرة، فإن الشبكة الخلوية غير متوفرة,
+ولكن يمكنك استخدام اتصالات wifi إذا كانت متوفرة.
+إذا كان جهازك يحتوي على آليات تقييد الوصول إلى الشبكة,
+تأكد من منح جميع الأذونات للتطبيق.
+تأكد من أن النظام ليس لديه أي قيود على الوصول إلى الإنترنت''',
+    '''Қазіргі уақытта wifi немесе ұялы байланыс жоқ.
+Интернетке қосылудың кем дегенде бір арнасын қамтамасыз етіңіз.
+Егер ұшақ режимі қосылған болса, онда ұялы желі қол жетімді емес,
+бірақ қол жетімді болса, wifi қосылымдарын пайдалануға болады.
+Егер сіздің құрылғыңызда желіге кіруді шектеу механизмдері болса,
+қолданбаға барлық рұқсаттардың берілгеніне көз жеткізіңіз.
+Жүйеде интернетке кіруге ешқандай шектеулер жоқ екеніне көз жеткізіңіз''',
+    '''Hozirda simsiz yoki uyali aloqa mavjud emas.
+Kamida bitta internetga kirish kanalini taqdim eting.
+Agar samolyot rejimi yoqilgan bo'lsa, u holda uyali tarmoq mavjud emas,
+agar mavjud bo'lsa, siz ulanishlardan foydalanishingiz mumkin.
+Agar qurilmangizda tarmoqqa kirishni cheklash mexanizmlari mavjud bo'lsa,
+ilovaga barcha ruxsatlar berilganligiga ishonch hosil qiling.
+Tizimda internetga kirishda hech qanday cheklovlar yo'qligiga ishonch hosil qiling''',
   ][_];
 }
